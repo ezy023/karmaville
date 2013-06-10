f052e89c42861f6f561a44f2cff58f0cf4cc888c
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-10 14:40:44 -0500

    pagination

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.12'
 gem 'thin'
+gem 'will_paginate', '~> 3.0'
 
 gem 'bcrypt-ruby'
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.limit(50)
+    @users = User.paginate(:page => params[:page], :per_page => 50)
     # user_ids = User.pluck(:id)
     # @users = User.group(:user).sum('karma_points')
   end
